a27044dae4b851dabd815e2d7b13bd7ce0d7f6a8
Oscarima <oscarima@free.fr>
2018-10-02 09:57:13 +0200

    Update from PC

--- Code/Sales/meteo.py
+++ Code/Sales/meteo.py
@@ -18,31 +18,49 @@ def loadMeteo(file):
     #convert to date only
     meteo.date=pd.to_datetime(meteo['date'], format='%Y%m%d%H0000',errors='coerce')
     #select only 12h
-    meteo=meteo[(meteo.date.dt.hour>8) & (meteo.date.dt.hour<=18)]
- 
- 
-    
+    meteo=meteo[(meteo.date.dt.hour>8) & (meteo.date.dt.hour<=18)]    
     meteo.date = (meteo["date"]).dt.strftime("%Y%m%d")
-    #meteo.date=meteo.date.dt.strftime("%Y%m%d")
     #selected value
     meteo['t']=pd.to_numeric(meteo['t'], errors='coerce')-273.15
     meteo['u']=pd.to_numeric(meteo['u'], errors='coerce')
     meteo['rrsum']=pd.to_numeric(meteo['rr3'], errors='coerce')
- 
-    #check null
-    #print  meteo.apply(lambda x: sum(x.isnull()),axis=0)
     #clean null value
     meteo=meteo[meteo.t.isnull()==False]
     meteo=meteo[meteo.u.isnull()==False]
     meteo=meteo[meteo.rrsum.isnull()==False]
-
     mean= meteo[['date','t','u']].groupby(['date'],as_index=False).mean()
-    rr=meteo[['date','rrsum']].groupby(['date'],as_index=False).sum()
- 
+    rr=meteo[['date','rrsum']].groupby(['date'],as_index=False).sum() 
     result = pd.merge(mean,rr)
     return result
 
-   
+
+def loadMeteo(file,num_sta):
+    meteo = pd.read_csv(file,sep=";")
+    meteo=meteo.loc[:,['numer_sta','date','t','u','rr3']]
+    #print meteo
+    #convert data
+    #filter only France
+    meteo.numer_sta=meteo.numer_sta.astype(str)
+    meteo=meteo[meteo.numer_sta==num_sta]
+
+    #convert to date only
+    meteo.date=pd.to_datetime(meteo['date'], format='%Y%m%d%H0000',errors='coerce')
+    #select only 12h
+    meteo=meteo[(meteo.date.dt.hour>8) & (meteo.date.dt.hour<=18)]    
+    meteo.date = (meteo["date"]).dt.strftime("%Y%m%d")
+    #selected value
+    meteo['t']=pd.to_numeric(meteo['t'], errors='coerce')-273.15
+    meteo['u']=pd.to_numeric(meteo['u'], errors='coerce')
+    meteo['rrsum']=pd.to_numeric(meteo['rr3'], errors='coerce')
+    #clean null value
+    meteo=meteo[meteo.t.isnull()==False]
+    meteo=meteo[meteo.u.isnull()==False]
+    meteo=meteo[meteo.rrsum.isnull()==False]
+    mean= meteo[['date','t','u']].groupby(['date'],as_index=False).mean()
+    rr=meteo[['date','rrsum']].groupby(['date'],as_index=False).sum() 
+    result = pd.merge(mean,rr)
+    return result
+
 def getMeteo(dt):
     uri="https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/synop.{0}.csv.gz".format(dt)
     response = u.urlopen(uri)
@@ -64,6 +82,14 @@ def buildMeteo(filename):
     result=pd.concat(frames)
     result.to_csv(filename,index=False)
 
+def buildMeteo(filename,num_sta):
+    frames = []
+    for fi in  glob.glob("meteo\synop.*"):
+        print "****Load {0} ****".format(fi)
+        frames.append(loadMeteo(fi,num_sta))
+    result=pd.concat(frames)
+    result.to_csv(filename,index=False)
+
 def loadAllMeteo(begin,end):
     dates = pd.date_range(start='20130101',end='20170101',freq='M')
     for dt in dates:
@@ -78,8 +104,5 @@ if __name__ == '__main__':
         buildMeteo("meteo.csv")
 
            #loadAllMeteo("20130101","20170101")
-
-
-
-#print res
-#caller.join(other.set_index('key'), on='key')
+    getMeteo("201809")
+    buildMeteo("meteoCorseAjacio.csv","7761")
